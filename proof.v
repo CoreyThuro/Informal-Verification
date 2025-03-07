87754a309b8c80b0a7c35a9a84d38a2e4e2f8e4f
Corey Thuro <coreythuro@Coreys-MBP.lan>
2025-03-07 16:28:42 -0500

    initial module push

--- proof.v
+++
@@ -1,9 +0,0 @@
-Require Import ZArith.
-
-Theorem sum_even: forall a b: Z, (exists k: Z, a = 2 * k) -> (exists m: Z, b = 2 * m) -> (exists n: Z, a + b = 2 * n).
-Proof.
-  intros a b [k Hk] [m Hm].
-  exists (k + m).
-  rewrite Hk, Hm.
-  ring.
-Qed.
